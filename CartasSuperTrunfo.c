0efea0615c13e5e833956d22367347985f48757a
Lantys <lantysu@gmail.com>
2025-10-29 01:08:23 +0000

    Corrigindo identação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,16 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+// Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado[20];
+    int cod_carta;
+    char nome_cidade[20]:
+    int popu;
+    float area;
+    float pib;
+    int pt_turis;
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
@@ -15,13 +22,7 @@ int main() {
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	char estado[20];
-    int cod_carta;
-    char nome_cidade[20]:
-    int popu;
-    float area;
-    float pib;
-    int pt_turis;
+	
     
 	
     return 0;
